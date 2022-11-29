1739ee2118b40bf5fa11f989113b5d2af10f115f
Gabriel Lamas <gabrielldarts@gmail.com>
2022-11-29 00:07:23 -0300

    born project square component

--- src/components/ProjectsComponent/index.js
+++ src/components/ProjectsComponent/index.js
@@ -1,9 +1,58 @@
 import ContainerProjectsComponent from "./style";
+import BuserLogo from "../../assets/logos/BuserLogo";
+import { useContext } from "react";
+import { ColorModeContext } from "../themeSwitcher/ColorModeProvider";
 
 function ProjectsComponent() {
+  const contexto = useContext(ColorModeContext);
   return (
     <ContainerProjectsComponent>
-      <div className="thumb-container"></div>
+      <div className="project-container">
+        <div className="moldure">
+          <div className="square">
+            <BuserLogo />
+          </div>
+          <div className="description">
+            <h4>Buser intern products</h4>
+            <p>
+              New visual for the app, adjustments on b2b dashboard and anothers
+              legacy screens.
+            </p>
+            <a href="">See more</a>
+          </div>
+        </div>
+      </div>
+      <div className="project-container">
+        <div className="moldure">
+          <div className="square">
+            <BuserLogo />
+          </div>
+          <div className="description">
+            <h4>Buser intern products</h4>
+            <p>
+              New visual for the app, adjustments on b2b dashboard and anothers
+              legacy screens.
+            </p>
+            <a href="">See more</a>
+          </div>
+        </div>
+      </div>
+
+      <div className="project-container">
+        <div className="moldure">
+          <div className="square">
+            <BuserLogo />
+          </div>
+          <div className="description">
+            <h4>Buser intern products</h4>
+            <p>
+              New visual for the app, adjustments on b2b dashboard and anothers
+              legacy screens.
+            </p>
+            <a href="">See more</a>
+          </div>
+        </div>
+      </div>
     </ContainerProjectsComponent>
   );
 }
--- src/components/ProjectsComponent/style.js
+++ src/components/ProjectsComponent/style.js
@@ -1,8 +1,19 @@
-import styled from "styled-components";
+import styled, { ThemeContext } from "styled-components";
 
 const ContainerProjectsComponent = styled.div`
   position: relative;
   flex: 1;
+  display: flex;
+  flex-direction: column;
+  gap: 80px;
+  .moldure {
+    display: flex;
+    border: 10px solid ${({ theme }) => theme.textColor900};
+    height: 400px;
+  }
+  .square {
+    background: #f11075;
+  }
 `;
 
 export default ContainerProjectsComponent;
