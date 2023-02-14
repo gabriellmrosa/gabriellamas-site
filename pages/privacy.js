aaf6a30067d48bd42a020d755c0b8422e933b6b8
Gabriel Lamas <gabrielldarts@gmail.com>
2023-02-14 16:40:48 -0300

    privacy ofertouapp

---
+++ pages/privacy.js
@@ -0,0 +1,24 @@
+export function PrivacyOfertou() {
+  return (
+    <p>
+      O Ofertou é um aplicativo que se preocupa com a privacidade dos seus
+      usuários e está comprometido em proteger suas informações pessoais. Essa
+      política de privacidade explica como coletamos, usamos, divulgamos e
+      protegemos as informações que você nos fornece através do aplicativo.
+      Coleta de informações O Ofertou não coleta informações sensíveis dos
+      usuários, tais como: dados bancários, senhas, informações de saúde, dados
+      de localização, entre outros. As única informação que coletamos é a sua
+      cidade para trazer as ofertas perto de você, essa informação não fica
+      salva em nosso banco de dados. Uso das informações As informações
+      coletadas são utilizadas apenas para fins de gerenciamento de sua conta no
+      aplicativo e para melhorar a sua experiência como usuário do Ofertou. Não
+      vendemos, alugamos ou compartilhamos suas informações pessoais com
+      terceiros. Divulgação de informações O Ofertou não divulga informações
+      pessoais do usuário, exceto se exigido por lei ou se acreditarmos de
+      boa-fé que a divulgação é necessária para proteger nossos direitos ou para
+      cumprir com um processo legal.
+    </p>
+  );
+}
+
+export default PrivacyOfertou;
